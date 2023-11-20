1b1b2bb4cec8eeb47f42b0b6952741afd882c958
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2023-11-20 12:08:07 -0600

    test: use testify assert for assertions

--- puzzles/day00/main_test.go
+++ puzzles/day00/main_test.go
@@ -1,17 +1,18 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestPart1(t *testing.T) {
 
 	t.Run("Part 1", func(t *testing.T) {
 
 		expenses := []int{1721, 979, 366, 299, 675, 1456}
-		actual := part1(expenses)
 		expected := 514579
-		if actual != expected {
-			t.Errorf("Expected %d but actual was %d", expected, actual)
-		}
+		actual := part1(expenses)
+		assert.Equal(t, expected, actual)
 	})
 
 }
@@ -21,11 +22,9 @@ func TestPart2(t *testing.T) {
 	t.Run("Part 2", func(t *testing.T) {
 
 		expenses := []int{1721, 979, 366, 299, 675, 1456}
-		actual := part2(expenses)
 		expected := 241861950
-		if actual != expected {
-			t.Errorf("Expected %d but actual was %d", expected, actual)
-		}
+		actual := part2(expenses)
+		assert.Equal(t, expected, actual)
 	})
 
 }
